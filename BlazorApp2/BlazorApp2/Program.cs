05577cffeb3bdc9f651738e8de27ee180e065b61
Kebin <kebinwang@gmail.com>
2024-10-20 12:03:30 -0400

    add blazor client

---
+++ BlazorApp2/BlazorApp2/Program.cs
@@ -0,0 +1,31 @@
+using BlazorApp2.Client.Pages;
+using BlazorApp2.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorApp2.Client._Imports).Assembly);
+
+app.Run();
